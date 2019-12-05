64bb1ff74b55f5b35fb44c478c89546806f8d575
nyamadori <nyamadorig@gmail.com>
2019-12-05 23:47:49 +0900

    symbolize parsed event payload json

--- action.rb
+++ action.rb
@@ -5,6 +5,6 @@ require 'active_support/all'
 
 require_relative './lib/metrics_comment_service'
 
-event_payload = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
+event_payload = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']), symbolize_names: true)
 
 MetricsCommentService.execute(event_payload)
